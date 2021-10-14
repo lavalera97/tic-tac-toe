d112057bd3998a99ba757cc0780f9ba3e9ac5e6e
Valery <fenixor0007@gmail.com>
2021-10-14 18:06:52 +0300

    Added interface for ai and player games

--- tictac.py
+++ tictac.py
@@ -18,50 +18,6 @@ class TicTacToe(tk.Tk):
         self._main_canvas = canvas
 
 
-class CreateBoard(tk.Canvas, ABC):
-    """Setting our base board to play with/ Создаём поле для игры"""
-    def __init__(self, master, *args, **kwargs):
-        super().__init__(master, *args, **kwargs)
-        self.pack(pady=(20, 60), padx=40)
-        self.board = {}
-        self.buttons = []
-        self.create_buttons()
-        self.create_board()
-        self.move_label = tk.Label(self, text="Choose side", font=('Segoe Print', 20, 'bold'), bd=4)
-        self.move_label.grid(column=10, row=4, padx=(30, 0))
-        # self.x_button = tk.Button(self, text="X", font=('Segoe Print', 15, 'bold'),
-        #                          bg='white', width=10, bd=4).grid(column=10, row=5, rowspan=2, padx=(30, 0))
-        # self.o_button =
-        self.name_label = tk.Label(self, text="", font=('Segoe Print', 20, 'bold'), bd=4)
-        self.name_label.grid(column=0, row=0, columnspan=10, pady=(0, 20))
-        self.restart = tk.Button(self, text="Restart", font=('Segoe Print', 14, 'bold'),
-                                 bg='white', width=10, bd=4, state=tk.DISABLED)
-        self.restart.grid(column=10, row=8, padx=(30, 0))
-        self.main_button = tk.Button(self, text="Main menu", font=('Segoe Print', 14, 'bold'), bd=4, bg='white',
-                                     width=10, command=lambda: master.switch_canvas(StartPage))
-        self.main_button.grid(column=10, row=9, rowspan=2, padx=(30, 0))
-        print(len(self.buttons))
-        print(self.board)
-
-    def create_board(self):
-        for i in range(100):
-            self.board[i] = ''
-
-    def create_buttons(self):
-        x = 0
-        for i in range(10):
-            for n in range(10):
-                self.buttons.append(x)
-                self.buttons[x] = tk.Button(
-                    self, text='', bd=4, height=3, width=6)
-                self.buttons[x].grid(row=i + 1, column=n)
-                x += 1
-
-    # @abstractmethod
-    # def choose_side(self):
-    #     pass
-
-
 class StartPage(tk.Canvas):
     """Creating starting page / Начальная страница"""
     def __init__(self, master, *args, **kwargs):
@@ -75,21 +31,91 @@ class StartPage(tk.Canvas):
         tk.Button(self, text="Exit", font=('Segoe Print', 15, 'bold'), bg='white', command=master.destroy).grid(column=0, row=3, pady=2)
 
 
+class CreateBoard(tk.Canvas, ABC):
+    """Setting our base board to play with/ Создаём поле для игры"""
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.master = master
+        self.pack(pady=(20, 60), padx=40)
+        self.board = {}
+        self.buttons = []
+        self.create_buttons()
+        self.create_board()
+        self.move_label = tk.Label(self, text="Choose side!", font=('Segoe Print', 20, 'bold'), bd=4)
+        self.move_label.grid(column=10, row=4, columnspan=2, padx=(32, 0))
+        self.name_label = tk.Label(self, text="", font=('Segoe Print', 20, 'bold'), bd=4)
+        self.name_label.grid(column=0, row=0, columnspan=10, pady=(0, 20))
+        self.restart = tk.Button(self, text="Restart", font=('Segoe Print', 14, 'bold'),
+                                 bg='white', width=10, bd=4, state=tk.DISABLED)
+        self.restart.grid(column=10, row=8, columnspan=2, padx=(30, 0))
+        self.main_button = tk.Button(self, text="Main menu", font=('Segoe Print', 14, 'bold'), bd=4, bg='white',
+                                     width=10, command=lambda: master.switch_canvas(StartPage))
+        self.main_button.grid(column=10, row=9, rowspan=2, columnspan=2, padx=(30, 0))
+
+    def create_board(self):
+        for i in range(100):
+            self.board[i] = ''
+
+    def create_buttons(self):
+        x = 0
+        for i in range(10):
+            for n in range(10):
+                self.buttons.append(x)
+                self.buttons[x] = tk.Button(
+                    self, text='', bd=4, height=3, width=6, state=tk.DISABLED)
+                self.buttons[x].grid(row=i + 1, column=n)
+                x += 1
+
+    @abstractmethod
+    def start_game(self, sign):
+        pass
+
+
 class PlayerVsPlayer(CreateBoard):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.name_label['text'] = 'Player Vs Player'
+        self.player_one = 'x'
+        self.player_two = 'y'
+        self.start_game('x')
 
-    # def choose_side(self):
-
+    def start_game(self, sign):
+        for i in self.buttons:
+            i['state'] = tk.NORMAL
+        self.restart.configure(command=lambda: self.master.switch_canvas(PlayerVsPlayer))
+        self.move_label['text'] = 'Player 1 Turn'
+        self.move_label.grid(column=10, row=4, rowspan=2, padx=(32, 0))
+        self.restart['state'] = tk.NORMAL
 
 
 class PlayerVsAi(CreateBoard):
-    def __init__(self, master, *args, **kwargs):
-        super().__init__(master, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.name_label['text'] = 'Player Vs AI'
+        self.player = ''
+        self.ai = ''
+        self.x_button = tk.Button(self, text="X", font=('Segoe Print', 13, 'bold'),
+                                 bg='white', height=0, width=4, bd=4, command=lambda: self.start_game('x'), fg="red")
+        self.x_button.grid(column=10, row=5, padx=(30, 0))
+        self.o_button = tk.Button(self, text="O", font=('Segoe Print', 13, 'bold'),
+                                 bg='white', height=0, width=4, bd=4, command=lambda: self.start_game('o'), fg="blue")
+        self.o_button.grid(column=11, row=5, padx=(30, 0))
 
-
+    def start_game(self, sign):
+        for i in self.buttons:
+            i['state'] = tk.NORMAL
+        self.x_button.grid_forget()
+        self.o_button.grid_forget()
+        self.restart['state'] = tk.NORMAL
+        self.restart.configure(command=lambda: self.master.switch_canvas(PlayerVsAi))
+        if sign == 'x':
+            self.player = 'x'
+            self.ai = 'o'
+            self.move_label['text'] = 'Your Turn'
+        if sign == 'o':
+            self.player = 'o'
+            self.ai = 'x'
+            self.move_label['text'] = 'AI thinking'
 
 
 if __name__ == '__main__':
